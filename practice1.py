1a8a894b268df051390d7a859e868e7e6e801cb2
dongja19 <kevin5472@naver.com>
2020-09-26 20:42:50 +0900

    intro

---
+++ practice1.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+a = 1
+b = 'apple'
+c = True
+d = ['apple', 'banana', 'watermelon']
+e = {'name' : 'donghyeon', 'age': '26'}
+f = ('apple', 'banana', 'watermelon')
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
